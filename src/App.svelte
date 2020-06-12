99df4029596c8b63e8f845364db9f52dde6dcdaa
Kayla Altepeter <kayla@kaylaaltepeter.com>
2020-06-12 15:19:14 -0500

    adding card and results

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,58 @@
 <script>
-import { onMount } from 'svelte';
-import Welcome from './screens/Welcome.svelte';
-import { select } from './select';
+  import { onMount } from "svelte";
+  import Welcome from "./screens/Welcome.svelte";
+  import Game from "./screens/Game.svelte";
+  import { select } from "./select";
 
-let celebs_promise;
+  let celebs_promise;
 
-  let state = 'welcome'; // 'welcome' or 'playing'
+  let state = "welcome"; // 'welcome' or 'playing'
   let selection;
 
   const start = async (e) => {
-	  const { celebs, lookup } = await celebs_promise;
+    const { celebs, lookup } = await celebs_promise;
 
-	  selection = select(celebs, lookup, e.detail.category.slug);
-	  state = 'playing';
+    selection = select(celebs, lookup, e.detail.category.slug);
+    state = "playing";
   };
 
   const load_celebs = async () => {
-	  const res = await fetch('https://cameo-explorer.netlify.app/celebs.json');
-	  const data = await res.json();
+    const res = await fetch("https://cameo-explorer.netlify.app/celebs.json");
+    const data = await res.json();
 
-	  const lookup = new Map();
+    const lookup = new Map();
 
-	  data.forEach(c => {
-		  lookup.set(c.id, c);
-	  });
+    data.forEach((c) => {
+      lookup.set(c.id, c);
+    });
 
-	  const subset = new Set();
-	  data.forEach(celeb => {
-		  if (celeb.reviews >= 50) {
-			  subset.add(celeb);
-			  celeb.similar.forEach(id => {
-				  subset.add(lookup.get(id));
-			  });
-		  }
-	  });
+    const subset = new Set();
+    data.forEach((celeb) => {
+      if (celeb.reviews >= 50) {
+        subset.add(celeb);
+        celeb.similar.forEach((id) => {
+          subset.add(lookup.get(id));
+        });
+      }
+    });
 
-	  return {
-		  celebs: Array.from(subset),
-		  lookup
-	  };
+    return {
+      celebs: Array.from(subset),
+      lookup,
+    };
   };
 
   onMount(() => {
-	  celebs_promise = load_celebs();
+    celebs_promise = load_celebs();
   });
 </script>
 
 <main>
-	{#if state === 'welcome'}
-		<Welcome on:select={start}/>
-	{:else if state === 'playing'}
-		<p>game screen goes here</p>
-	{/if}
+  {#if state === 'welcome'}
+    <Welcome on:select={start} />
+  {:else if state === 'playing'}
+    <Game {selection} />
+  {/if}
 </main>
 
 <style>
